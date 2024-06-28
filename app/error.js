a4ce23b48b943e2da5a1dca3612ba63b651e2b26
osama <osamaabdullahimohammed@gmail.com>
2024-06-28 17:32:00 +0300

    added an error handler

---
+++ app/error.js
@@ -0,0 +1,21 @@
+// pages/_error.js
+import React from "react";
+import Error from "next/error";
+
+const CustomError = ({ statusCode }) => {
+  return (
+    <div>
+      <h1>{statusCode} - An error occurred</h1>
+      <p>
+        {statusCode === 404 ? "Page not found" : "An unexpected error occurred"}
+      </p>
+    </div>
+  );
+};
+
+CustomError.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default CustomError;
